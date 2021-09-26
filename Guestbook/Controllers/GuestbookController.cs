7d2bb511b72f1b00e12596a062dce17d7a6e1f02
Bruno Campos <30634714+brunocamps@users.noreply.github.com>
2021-09-26 10:11:19 -0300

    Initial commit

---
+++ Guestbook/Controllers/GuestbookController.cs
@@ -0,0 +1,43 @@
+﻿using Guestbook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Guestbook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        private GuestbookContext _db = new GuestbookContext();
+
+        public ActionResult Index()
+        {
+            var mostRecentEntries =
+                (from entry in _db.Entries
+                 orderby entry.DateAdded descending 
+                 select entry).Take(20);
+
+            ViewBag.Entries = mostRecentEntries.ToList();
+            return View();
+        }
+
+
+        // GET: Guestbook
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // restrict access to HTTP post [HttpPost] is a decorator. Accept GuestbookEntry as parameter
+        public ActionResult Create(GuestbookEntry entry) // Method overloading (Originally Create() above)
+        {
+            entry.DateAdded = DateTime.Now;
+            _db.Entries.Add(entry);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index"); // because Index is an action
+        }
+    }
+}
---
+++ Guestbook/Models/GuestbookContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Guestbook.Models
+{
+    public class GuestbookContext : DbContext
+    {
+        public GuestbookContext() : base("Guestbook")
+        {
+
+        }
+        public DbSet<GuestbookEntry> Entries { get; set; }
+    }
+}
